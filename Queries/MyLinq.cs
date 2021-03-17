aca83b4ab126d627d81881625dc6b1f85bcd8e4a
Gustavo Sampaio <sampaioglima@gmail.com>
2021-03-17 19:01:08 -0300

    Streaming-enabled operators to boost your query's performance

--- Queries/MyLinq.cs
+++ Queries/MyLinq.cs
@@ -1,10 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace Queries
 {
     public static class MyLinq
     {
+        public static IEnumerable<double> Ramdom()
+        {
+            var random = new Random();
+            while (true)
+            {
+                yield return random.NextDouble();
+            }
+        }
+
         public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate)
         {
             foreach (var item in source)
--- Queries/Program.cs
+++ Queries/Program.cs
@@ -8,6 +8,13 @@ namespace Queries
     {
         public static void Main()
         {
+            var numbers = MyLinq.Ramdom().Where(r => r > 0 && r < 1).Take(8);
+            
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
             var movies = new List<Movie>()
             {
                 new Movie { Title = "The Dark Knight", Rating = 8.9f, Year = 2008 },
@@ -15,11 +22,9 @@ namespace Queries
                 new Movie { Title = "Casablanca", Rating = 8.5f, Year = 1942 },
                 new Movie { Title = "Star Wars V", Rating = 8.7f, Year = 1980 }
             };
-            // building the structure, but it won't do anything until
-            // some code triggers LINQ to perform the query.
-            var query = movies.Where(m => m.Year > 2000).ToList();
 
-            Console.WriteLine($"{query.Count()} <- Movies that match the condition");
+            var query = movies.Where(m => m.Year > 2000)
+                              .OrderBy(m => m.Rating);
 
             var enumerator = query.GetEnumerator();
             while (enumerator.MoveNext() == true)
